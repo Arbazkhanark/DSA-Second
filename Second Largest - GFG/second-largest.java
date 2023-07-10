b87b3fc0f1fbbbb6c96a1dd7ea1f0af970b960f0
Arbaaz Khan <79525841+Arbazkhanark@users.noreply.github.com>
2023-07-10 15:27:31 +0530

    Added solution - LeetHub

---
+++ Second Largest - GFG/second-largest.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans = new Solution().print2largest(arr, n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int print2largest(int arr[], int n) {
+        
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<min){
+                min=arr[i];
+            }
+        }
+        
+        int max=min;
+        int secondMax=-1;
+        
+        Arrays.sort(arr);
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                
+                secondMax=max;
+                max=arr[i];
+            }
+        }
+        return secondMax;
+    }
+}
