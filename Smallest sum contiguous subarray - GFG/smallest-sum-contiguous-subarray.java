47d2f0a768c2ea90bbe317fd7cd12c8fbf8c1dca
Arbaaz Khan <79525841+Arbazkhanark@users.noreply.github.com>
2023-07-12 16:40:52 +0530

    Added solution - LeetHub

---
+++ Smallest sum contiguous subarray - GFG/smallest-sum-contiguous-subarray.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Geeks
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n =sc.nextInt();
+            int arr[] = new int[n];
+            
+            for(int i = 0; i < n; i++)
+             arr[i] = sc.nextInt();
+             
+           System.out.println(new Solution().smallestSumSubarray(arr, n)); 
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution
+{
+    static int smallestSumSubarray(int arr[], int size){
+        int sum=0;
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+            if(min>sum){
+                min=sum;
+            }
+            
+            if(sum>0){
+                sum=0;
+            }
+        }
+        
+        return min;
+    }
+}
