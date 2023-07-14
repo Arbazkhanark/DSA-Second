2fdb028dbbaa69d991993b13f72c97cafe175b59
Arbaaz Khan <79525841+Arbazkhanark@users.noreply.github.com>
2023-07-15 05:04:22 +0530

    Added solution - LeetHub

---
+++ Rotate by 90 degree - GFG/rotate-by-90-degree.java
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t= Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            int n = Integer.parseInt(read.readLine());
+            int matrix[][] = new int[n][n];
+            String st[] = read.readLine().trim().split("\\s+");
+            int k = 0;
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                    matrix[i][j] = Integer.parseInt(st[k++]);
+            }
+            Solution ob = new Solution();
+            ob.rotateby90(matrix, n);
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                    System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to rotate matrix anticlockwise by 90 degrees.
+    static void rotateby90(int arr[][], int n) { 
+        
+        for(int i=0;i<arr.length;i++){
+            for(int j=i;j<arr[0].length;j++){
+                int temp=arr[i][j];
+                arr[i][j]=arr[j][i];
+                arr[j][i]=temp;
+            }
+        }
+        
+
+        for(int j=0;j<arr.length;j++){
+            int left=0;
+            int right=arr.length-1;
+            while(left<right){
+                // if(left<right){
+                    int temp=arr[left][j];
+                    arr[left][j]=arr[right][j];
+                    arr[right][j]=temp;
+                    left++;
+                    right--;
+                // }
+            }
+            
+
+        }
+        
+
+    }
+}
