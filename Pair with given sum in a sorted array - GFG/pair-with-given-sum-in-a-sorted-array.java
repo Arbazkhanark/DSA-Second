8e3ab762c5fc652f435e01f20a6ee7724146bba7
Arbaaz Khan <79525841+Arbazkhanark@users.noreply.github.com>
2023-07-21 01:16:25 +0530

    Added solution - LeetHub

---
+++ Pair with given sum in a sorted array - GFG/pair-with-given-sum-in-a-sorted-array.java
@@ -0,0 +1,89 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+//Initial Template for Java
+
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+            
+
+class Solution{
+    
+  
+    int Countpair(int arr[], int n, int sum){
+        
+        int count=0;
+        int left=0;
+        int right=arr.length-1;
+
+        while(left<right){
+            int tempSum=arr[left]+arr[right];
+            if(sum==tempSum){
+                count++;left++;right--;
+            }else if(tempSum>sum){
+                right--;
+            }else if(tempSum<sum){
+                left++;
+            }
+        }
+        
+        return count<1 ? -1:count;
+    }
+  
+    
+}
+
+
+//{ Driver Code Starts.
+
+// Driver class
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    
+		    String line1 = br.readLine();
+		    String[] element = line1.trim().split("\\s+");
+		    int sizeOfArray = Integer.parseInt(element[0]);
+		    
+		    int arr [] = new int[sizeOfArray];
+		    
+		    String line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    int K = Integer.parseInt(br.readLine());
+		    
+		    Solution obj = new Solution();
+		   
+		    int res = obj.Countpair(arr, sizeOfArray, K);
+		    
+		    System.out.println(res);
+		    
+		    
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
