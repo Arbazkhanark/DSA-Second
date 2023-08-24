5d1a7008177657316184af427238038ef7abc9f9
Arbaaz Khan <79525841+Arbazkhanark@users.noreply.github.com>
2023-08-25 01:15:04 +0530

    Added solution - LeetHub

---
+++ Trailing zeroes in factorial - GFG/trailing-zeroes-in-factorial.java
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            int N = sc.nextInt();
+			
+            Solution ob = new Solution();
+            int ans  = ob.trailingZeroes(N);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static int trailingZeroes(int n){        
+        
+        int count = 0;
+        while(n>=5){
+            n=n/5;
+            count=count+n;
+        }
+        return count;
+    }
+}
