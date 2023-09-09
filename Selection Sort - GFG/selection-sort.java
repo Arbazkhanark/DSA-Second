725ae0836638026d6139eef3fba3ee2563546bef
Arbaaz Khan <79525841+Arbazkhanark@users.noreply.github.com>
2023-09-09 22:57:46 +0530

    Added solution - LeetHub

--- Selection Sort - GFG/selection-sort.java
+++ Selection Sort - GFG/selection-sort.java
@@ -31,17 +31,13 @@ class GFG
 // } Driver Code Ends
 
 
-class Solution
-{
-// 	int  select(int arr[], int i){
-        
-// 	}
-	
+class Solution{
+    
 	void selectionSort(int arr[], int n){
 	    
 	    for(int i=0;i<arr.length;i++){
 	        int min=arr[i];
-	        for(int j=i+1;j<arr.length;j++){
+	        for(int j=i;j<arr.length;j++){
 	            if(min>arr[j]){
 	                min=arr[j];
 	                int temp=arr[i];
@@ -50,5 +46,6 @@ class Solution
 	            }
 	        }
 	    }
+	    
 	}
 }
