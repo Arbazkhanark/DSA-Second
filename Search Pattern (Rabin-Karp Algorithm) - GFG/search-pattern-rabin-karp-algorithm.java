779b4e412694060f5a4f6992feeaaba8dd08d1c0
Arbaaz Khan <79525841+Arbazkhanark@users.noreply.github.com>
2023-09-11 00:30:46 +0530

    Added solution - LeetHub

--- Search Pattern (Rabin-Karp Algorithm) - GFG/search-pattern-rabin-karp-algorithm.java
+++ Search Pattern (Rabin-Karp Algorithm) - GFG/search-pattern-rabin-karp-algorithm.java
@@ -31,32 +31,44 @@ class GFG
 
 //User function Template for Java
 
-class Solution
-{
-    
-    ArrayList<Integer> search(String pat, String str){
-        ArrayList<Integer> list=new ArrayList<>();
-        
-        int n1=str.length()-1;
-        int n2=pat.length()-1;
-        
-        
-        for(int i=0;i<=n1-n2;i++){
-        int count=0;
-            for(int j=0;j<=n2;j++){
-                if(pat.charAt(j)!=str.charAt(i+j)){
-                    break;
+class Solution {
+
+    ArrayList<Integer> search(String pat, String str) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        int n1 = str.length();
+        int n2 = pat.length();
+
+        int pSum=0;
+        int sSum=0;
+
+        for(int i=0;i<n2;i++){
+            pSum +=pat.charAt(i);
+            sSum +=str.charAt(i);
+        }
+
+        for(int i=n2-1;i<n1;i++){
+            int count = 0;
+
+            if (pSum == sSum) {
+                for (int j=0;j<n2;j++){
+                    if (pat.charAt(j) != str.charAt(i-n2+1+j)) {
+                        break;
+                    } else {
+                        count++;
+                    }
                 }
-                else{
-                    count++;
+                if (count == n2){
+                    list.add(i+1-n2+1);
                 }
             }
-            if(count==n2+1){
-                list.add(i+1);
+
+            if(i<n1-1){
+                sSum =sSum-str.charAt(i-n2+1)+str.charAt(i+1);
             }
         }
         
-        if(list.size()==0){
+        if(list.size()<1){
             list.add(-1);
         }
         
