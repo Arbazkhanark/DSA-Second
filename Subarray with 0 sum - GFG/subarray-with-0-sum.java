e1c4c6ad3b01598ba74b7c849d68f4a680ed0f63
Arbaaz Khan <79525841+Arbazkhanark@users.noreply.github.com>
2023-09-30 23:27:34 +0530

    Added solution - LeetHub

--- Subarray with 0 sum - GFG/subarray-with-0-sum.java
+++ Subarray with 0 sum - GFG/subarray-with-0-sum.java
@@ -43,21 +43,19 @@ class GFG {
 class Solution{
     //Function to check whether there is a subarray present with 0-sum or not.
     static boolean findsum(int arr[],int n){
-        
         HashSet<Integer> set=new HashSet<>();
-        
-        int sum=0;
+        int cs=0;
         for(int i=0;i<arr.length;i++){
-            sum+=arr[i];
-
-            if(sum==0 || set.contains(sum)){
+            cs+=arr[i];
+            if(cs==0){
                 return true;
-            }else{
-                set.add(sum);
             }
-            
+            if(!set.contains(cs)){
+                set.add(cs);
+            }else{
+                return true;
+            }
         }
-        
         return false;
     }
 }
