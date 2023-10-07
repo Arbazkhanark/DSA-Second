56686bb63273eb99b15a45e2fb02d127de7ac65e
Arbaaz Khan <79525841+Arbazkhanark@users.noreply.github.com>
2023-10-07 23:38:13 +0530

    Added solution - LeetHub

---
+++ Insert in Middle of Linked List - GFG/insert-in-middle-of-linked-list.java
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class Node {
+    int data;
+    Node next;
+    
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+        int t = sc.nextInt();
+        
+        while(t-->0){
+            int n = sc.nextInt();   
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            
+            for(int i=0; i<n-1; i++){
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+            int key = sc.nextInt();
+            
+            //display(head);
+            Solution obj = new Solution();
+            obj.insertInMid(head, key);
+            display(head);
+            System.out.println();
+        }
+    }
+    
+    public static void display(Node head){
+        Node curr = head;
+        while(curr != null){
+            System.out.print(curr.data+" ");
+            curr = curr.next;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*
+Structure of node class is:
+class Node {
+    int data;
+    Node next;
+    
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+*/
+class Solution {
+    
+    public Node insertInMid(Node head, int data){
+        Node fast=head;
+        Node slow=head;
+        
+        while(fast!=null && fast.next != null && fast.next.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+        }
+        
+        Node NewData=new Node(data);
+        
+        NewData.next=slow.next;
+        slow.next=NewData;
+        
+        return head;
+    }
+}
